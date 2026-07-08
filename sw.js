d99b0118b2e2a5fd223b5019ddc80538ff7f6602
tysonbruv <tysonmuru@gmail.com>
2026-07-08 12:36:33 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-const CACHE_NAME = "road-discovery-au-v19";
+const CACHE_NAME = "road-discovery-au-v20";
 
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css",
-  "./app.js",
+  "./style.css?v=20",
+  "./app.js?v=20",
   "./manifest.json",
   "./icon.svg",
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
@@ -17,22 +19,37 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((k) => k !== CACHE_NAME)
-          .map((k) => caches.delete(k))
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
       )
-    )
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
 
-  if (url.origin === location.origin) {
-    event.respondWith(
-      caches.match(event.request).then((cached) => cached || fetch(event.request))
-    );
+  if (url.origin !== location.origin) {
+    return;
   }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((networkResponse) => {
+        const copy = networkResponse.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, copy);
+        });
+
+        return networkResponse;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
