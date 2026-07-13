8d065cc03d4e4fa1654975a79bab2ca1b0dde019
tysonbruv <tysonmuru@gmail.com>
2026-07-13 22:43:51 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "road-discovery-au-v28";
+const CACHE_NAME = "road-discovery-au-v29";
 
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=28",
-  "./app.js?v=28",
+  "./style.css?v=29",
+  "./app.js?v=29",
   "./manifest.json",
   "./icon.svg",
 ];
