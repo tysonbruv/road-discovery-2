3880bdaf621633b759053c683652965943aedfbd
tysonbruv <tysonmuru@gmail.com>
2026-09-25 19:14:30 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 "use strict";
 
 /*
-  Road Discovery AU v169 service worker
+  Road Discovery AU v170 service worker
 
   Expected frontend versions:
-  - app.js?v=169
+  - app.js?v=170
   - style.css?v=68
 */
 
 const CACHE_NAME =
-  "road-discovery-au-v169";
+  "road-discovery-au-v170";
 
 const CORE_APP_SHELL = [
   "./",
@@ -20,6 +20,7 @@ const CORE_APP_SHELL = [
 
 const VERSIONED_APP_FILES = [
   "./style.css?v=68",
+  "./app.js?v=170",
   "./app.js?v=169",
   "./app.js?v=168",
   "./app.js?v=167",
@@ -254,6 +255,11 @@ self.addEventListener(
             )
           ) {
             return (
+              await caches.match(
+                "./app.js?v=170"
+              )
+            ) ||
+            (
               await caches.match(
                 "./app.js?v=169"
               )
